453800521b1ebb210bec2a7fe080b07b3978947a
d4niel <olayajuandaniel8@gmail.com>
2022-10-13 17:49:13 -0500

    Search user

---
+++ src/interfaces/authentication.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface IAuthenticacionRepository<TModel>
+{
+    verifyUser(email:string):Promise<TModel>;
+    
+    verifySession(email:string, password:string):Promise<TModel>;
+    
+}
+
+
+
+
---
+++ src/repositories/autenticacion.repository.ts
@@ -0,0 +1,26 @@
+import { IAuthenticacionRepository } from "../interfaces/authentication.interface";
+import { usuarios } from "@prisma/client";
+import prisma from "../database/database";
+
+class AuthenticacionRepository implements IAuthenticacionRepository<usuarios>
+{
+    async verifyUser(email:string): Promise<usuarios> {
+        const user:any = await prisma.usuarios.count({
+            where:{
+                email:email
+            }
+        })
+        return user;
+    }
+    async verifySession(email:string , password:string): Promise<usuarios> {
+        const user:any = await prisma.usuarios.count({
+            where:{
+                AND:{
+                    email:email,
+                    password:password
+                }
+            }
+        })
+        return user;
+    }
+}
